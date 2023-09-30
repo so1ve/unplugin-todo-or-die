b5ce05bf9f9ba6d3c77027299bb20ae1e6b1b937
Ray <i@mk1.io>
2023-09-30 14:14:25 +0800

    refactor: use regexp

--- src/core/transform.ts
+++ src/core/transform.ts
@@ -1,34 +1,26 @@
-import type { ParserOptions } from "@babel/parser";
-import { parse } from "@babel/parser";
-import traverse from "@babel/traverse";
-import type { Comment, CommentLine } from "@babel/types";
 import MagicString from "magic-string";
 
-const filterComments = (comment: Comment): comment is CommentLine =>
-	comment.type === "CommentLine";
 
 // eslint-disable-next-line regexp/no-super-linear-backtracking
-const todoReg = /^\s*TODO::expires?\((\d{4}-\d{2}-\d{2})\):\s*(.*)$/;
+const todoReg = /^\s*//\s*TODO::expires?\((\d{4}-\d{2}-\d{2})\):\s*(.*)$/g;
 
 interface Todo {
 	expires: number;
 	content: string;
-	comment: CommentLine;
+	start:number
+	end: number
 }
 
-function parseComment(comment: CommentLine): Todo | undefined {
-	const match = comment.value.match(todoReg);
-	if (!match) {
-		return;
-	}
-	const [, date, content] = match;
-	if (match && date) {
+function parseComment(match: RegExpExecArray): Todo | undefined {
+	
+	const [comment, date, content] = match;
+	if ( date) {
 		const expires = new Date(date).getTime();
 
 		return {
 			expires,
 			content,
-			comment,
+			start:match.index,end:match.index+comment.length
 		};
 	}
 }
@@ -36,58 +28,15 @@ function parseComment(comment: CommentLine): Todo | undefined {
 const generateDieCode = (todo: Todo) =>
 	`if (Date.now() > ${todo.expires}) throw new Error("TODO expired: ${todo.content}");`;
 
-const getParseOptions = (isJsx: boolean): ParserOptions => ({
-	sourceType: "module",
-	allowImportExportEverywhere: true,
-	allowReturnOutsideFunction: true,
-	allowNewTargetOutsideFunction: true,
-	allowSuperOutsideMethod: true,
-	allowUndeclaredExports: true,
-	errorRecovery: true,
-	plugins: [
-		"doExpressions",
-		"exportDefaultFrom",
-		"functionBind",
-		"functionSent",
-		"throwExpressions",
-		"partialApplication",
-		"decorators",
-		"decimal",
-		"moduleBlocks",
-		"asyncDoExpressions",
-		"regexpUnicodeSets",
-		"destructuringPrivate",
-		"decoratorAutoAccessors",
-		"importReflection",
-		"explicitResourceManagement",
-		"decoratorAutoAccessors",
-		"typescript",
-		...(isJsx ? ["jsx" as const] : []),
-		["importAttributes", { deprecatedAssertSyntax: true }],
-	],
-});
+
 
 export function transform(code: string, id: string) {
 	const s = new MagicString(code);
-	const parsed = parse(code, getParseOptions(/(?:js|x)$/.test(id)));
-	const comments: CommentLine[] = [];
-
-	traverse(parsed, {
-		enter({ node }) {
-			comments.push(
-				...[
-					...(node.leadingComments ?? []),
-					...(node.innerComments ?? []),
-					...(node.trailingComments ?? []),
-				].filter(filterComments),
-			);
-		},
-	});
-
-	const todos = comments.map(parseComment).filter(Boolean) as Todo[];
+		const matches=code.matchAll(todoReg)
+	const todos = matches.map(parseComment).filter(Boolean) as Todo[];
 
 	for (const todo of todos) {
-		s.overwrite(todo.comment.start!, todo.comment.end!, generateDieCode(todo));
+		s.overwrite(todo.start, todo.end, generateDieCode(todo));
 	}
 
 	return {
